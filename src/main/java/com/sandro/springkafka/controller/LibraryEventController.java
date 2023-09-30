5a13934f7976c223e736bafd6917ac5dea7ad4e7
sandropark <ticoba@naver.com>
2023-09-30 16:55:32 +0900

    [추가] 프로듀스 API 추가 및 프로듀싱 테스트

--- src/main/java/com/sandro/springkafka/controller/LibraryEventController.java
+++ src/main/java/com/sandro/springkafka/controller/LibraryEventController.java
@@ -1,6 +1,8 @@
 package com.sandro.springkafka.controller;
 
 import com.sandro.springkafka.domain.LibraryEvent;
+import com.sandro.springkafka.producer.LibraryEventsProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +11,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 public class LibraryEventController {
+
+    private final LibraryEventsProducer libraryEventsProducer;
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
         log.info("libraryEvent = {}", libraryEvent);
+        libraryEventsProducer.send(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
