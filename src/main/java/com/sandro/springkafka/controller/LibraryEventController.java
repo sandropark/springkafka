bfb3b656a9942254641c5fa86a9e71b940c3d5d8
sandropark <ticoba@naver.com>
2023-10-01 13:25:54 +0900

    [추가] UPDATE 엔드포인트 추가

--- src/main/java/com/sandro/springkafka/controller/LibraryEventController.java
+++ src/main/java/com/sandro/springkafka/controller/LibraryEventController.java
@@ -1,6 +1,7 @@
 package com.sandro.springkafka.controller;
 
 import com.sandro.springkafka.domain.LibraryEvent;
+import com.sandro.springkafka.domain.LibraryEventType;
 import com.sandro.springkafka.producer.LibraryEventsProducer;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +21,31 @@ public class LibraryEventController {
     private final LibraryEventsProducer libraryEventsProducer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> create(@RequestBody @Valid LibraryEvent libraryEvent) {
         log.info("libraryEvent = {}", libraryEvent);
 //        libraryEventsProducer.send(libraryEvent);
 //        libraryEventsProducer.send_producerRecord(libraryEvent);
         libraryEventsProducer.send_sync(libraryEvent);
-        log.info("After Sending libraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> update(@RequestBody @Valid LibraryEvent libraryEvent) {
+        log.info("libraryEvent = {}", libraryEvent);
+
+        ResponseEntity<String> BAD_REQUEST = validateLibraryEvent(libraryEvent);
+        if(BAD_REQUEST != null) return BAD_REQUEST;
+
+        libraryEventsProducer.send_sync(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
+
+    private ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent) {
+        if (libraryEvent.libraryEventId() == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibarayEventId");
+
+        if (!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE event type is supported");
+        return null;
     }
 }
